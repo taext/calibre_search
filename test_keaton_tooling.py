440eb9801b0ca77a04c45d0534a851700890d1c1
David <gh@v1d.dk>
2019-05-28 13:02:50 +0200

    add test_repr

--- test_keaton_tooling.py
+++ test_keaton_tooling.py
@@ -4,19 +4,31 @@ import unittest
 class TestCalibreTooling(unittest.TestCase):
 
     def test_dict_loading(self):
-        books = kt.build_dict('keaton.csv')                                                                                                
+        #books = kt.build_dict('keaton.csv')                                                                                                
 
-        self.assertEqual(len(books), 4245)
+        self.assertEqual(len(kt.books), 4245)
 
     def test_searching(self):
 
-        books = kt.build_dict('keaton.csv')                                                                                                
-        spanish_search = books.search('spanish','tags')                                                                                    
+        #books = kt.build_dict('keaton.csv')                                                                                                
+        spanish_search = kt.books.search('spanish','tags')                                                                                    
         self.assertEqual(len(spanish_search), 13)
 
     def test_secondary_search(self):
 
-        books = kt.build_dict('keaton.csv')                                                                                                
-        spanish_search = books.search('spanish','tags')                                                                                    
+        #books = kt.build_dict('keaton.csv')                                                                                                
+        spanish_search = kt.books.search('spanish','tags')                                                                                    
         second_search = spanish_search.search('science','tags')
         self.assertEqual(len(second_search), 2)
+
+    def test_repr(self):
+        german_search = kt.books.search('german','tags')
+        string_repr = german_search.__repr__()
+        
+
+        result_to_check_for = """Basic German: A Grammar And Workbook (Grammar Workbooks)
+Reaktionen Der Organischen Chemie
+Rfid
+S-Ketamin - Aktuelle Interdisziplinäre Aspekte"""
+
+        self.assertEqual(string_repr[:142], result_to_check_for)
